e848591ce77fff51dcb0dcde4800d385e177535e
J. David Ibáñez <jdavid.ibp@gmail.com>
2021-09-23 09:14:22 +0200

    Security update

--- project/settings.py
+++ project/settings.py
@@ -70,7 +70,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     # Wagtail
-    'wagtail.core.middleware.SiteMiddleware',
     'wagtail.contrib.redirects.middleware.RedirectMiddleware',
 ]
 
